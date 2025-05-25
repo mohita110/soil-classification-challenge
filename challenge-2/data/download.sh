bb238e146eb22a656d54d9a54db6f98b1245ab92
siya21k <siyak.kumar21@gmail.com>
2025-05-25 23:23:43 +0530

    Update download.sh

--- challenge-2/data/download.sh
+++ challenge-2/data/download.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+set -e
 
-# Dataset or competition or notebook slug
-# dataset: 'soil-classification'
-# competition: 'soil-classification'
-# notebook: 'mohita/soil_classification'
-
-# === Download a dataset ===
-KAGGLE_DATASET="soilclassification-part2"
+# ---- Soil Dataset Download ----
+KAGGLE_DATASET="annam-ai/soilclassification"
 TARGET_DIR="./data"
 
 echo "Downloading dataset: $KAGGLE_DATASET"
 mkdir -p "$TARGET_DIR"
-kaggle competitions download -c "$KAGGLE_COMPETITION" -p "$TARGET_DIR" --unzip
+kaggle datasets download -d "$KAGGLE_DATASET" -p "$TARGET_DIR" --unzip
 
 echo "Download complete. Files saved to $TARGET_DIR"
+# ---not soil - cifar 10 ---Download
+pip install --quiet gdown
+
+GDRIVE_FOLDER_ID="1H95RE3o0j1n50yj3MSvRSCEVZKN9jH-G"
+gdown --folder "https://drive.google.com/drive/folders/1H95RE3o0j1n50yj3MSvRSCEVZKN9jH-G?usp=sharing" -O data/not-soil
+
+
+
+echo "All downloads completed."
